786a4f10d4b351a101229841990da17a538dff6a
Nevvord <nevvord@gmail.com>
2019-10-02 00:51:04 +0300

    Start + add i18n

---
+++ components/NavBar/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="light" variant="white">
+            <b-navbar-brand to="#">
+                {{ $t('logoName') }}
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-colapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-colapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item to="#">{{ $t('navBar.firstLink') }}</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NavBar />
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar/Navbar'
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
